b97cbad42af771e605374583692af25ef37387d7
Leidey <tunahan.yediel@gmail.com>
2017-03-20 15:46:45 +0200

    Update Dockerize.sh

--- Dockerize.sh
+++ Dockerize.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
-docker login -u tyediel -p $DOCKER_PASSWORD
+#docker login -u tyediel -p $DOCKER_PASSWORD
 docker build -t tyediel/devops:$BUILD_NUMBER .
 docker push tyediel/devops:$BUILD_NUMBER
